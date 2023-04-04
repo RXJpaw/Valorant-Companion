b1f9142a991b8aaa520b034d390c32a327605379
RXJpaw <github@rxj.pw>
2023-04-04 14:49:05 +0200

    🏷️ Included interface for Valorant API-Seasons.

---
+++ interfaces/valorant_api/seasons.d.ts
@@ -0,0 +1,9 @@
+interface ValorantAPISeason {
+    uuid: string
+    displayName: string
+    type?: string
+    startTime: string
+    endTime: string
+    parentUuid?: string
+    assetPath: string
+}
