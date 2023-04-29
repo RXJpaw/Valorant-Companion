52eab782465979c73355f82efa4e3b30ae83a0c6
RXJpaw <github@rxj.pw>
2023-04-29 22:24:35 +0200

    🏷️ Included type-declarations for player-cards- and player-titles-endpoint.

---
+++ interfaces/valorant_api/playercards.d.ts
@@ -0,0 +1,11 @@
+interface ValorantAPIPlayerCard {
+    uuid: string
+    displayName: string
+    isHiddenIfNotOwned: boolean
+    themeUuid?: any
+    displayIcon: string
+    smallArt: string
+    wideArt: string
+    largeArt: string
+    assetPath: string
+}
---
+++ interfaces/valorant_api/playertitles.d.ts
@@ -0,0 +1,7 @@
+interface ValorantAPIPlayerTitle {
+    uuid: string
+    displayName: string
+    titleText: string
+    isHiddenIfNotOwned: boolean
+    assetPath: string
+}
