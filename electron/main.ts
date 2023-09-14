40df28de3507ab9b8c06d30f58c947c01d55e208
RXJpaw <github@rxj.pw>
2023-09-14 19:51:49 +0200

    🚧 Some tray tests. (commented out)

--- electron/main.ts
+++ electron/main.ts
@@ -3,8 +3,9 @@ const { app } = require('electron')
 if (require('electron-squirrel-startup')) app.quit()
 
 // Modules to control application life and create native browser window
-import { BrowserWindow, protocol, ipcMain, shell, globalShortcut, dialog } from 'electron'
+import { BrowserWindow, protocol, ipcMain, shell, globalShortcut, dialog, Menu, Tray } from 'electron'
 import { createProtocol, updateProtocolResourceName } from './createProtocol'
+import { setTimeout as sleep } from 'timers/promises'
 import { getVersions } from './methods'
 import path from 'path'
 import fs from 'fs'
@@ -23,6 +24,8 @@ protocol.registerSchemesAsPrivileged([{ scheme: customProtocol, privileges: { se
 process.env.ELECTRON_DISABLE_SECURITY_WARNINGS = '1'
 
 let mainWindow = null as never as BrowserWindow
+let trayWindow = null as never as BrowserWindow
+let trayIcon = null as never as Tray
 
 async function createWindow() {
     // Create the browser window.
@@ -44,15 +47,39 @@ async function createWindow() {
         frame: false
     })
 
-    //TODO: after updating electron to 21.3.5, Ctrl+R would stop working
+    // trayWindow = new BrowserWindow({
+    //     width: 200,
+    //     height: 300,
+    //     skipTaskbar: true,
+    //     autoHideMenuBar: true,
+    //     webPreferences: {},
+    //     show: false,
+    //     frame: false
+    // })
+    //
+    // trayWindow.on('blur', () => {
+    //     trayWindow.hide()
+    // })
+    //
+    // trayIcon = new Tray(path.resolve(__dirname, '../public/favicon.ico'))
+    // trayIcon.setToolTip('Valorant Companion')
+    //
+    // trayIcon.on('click', async (event, bounds) => {
+    //     await sleep(50)
+    //
+    //     const WindowSize = trayWindow.getSize()
+    //
+    //     trayWindow.setPosition(bounds.x - WindowSize[0] + bounds.width, bounds.y - WindowSize[1])
+    //     trayWindow.show()
+    // })
 
-    mainWindow.on('focus', () => {
-        globalShortcut.register('CommandOrControl+R', () => mainWindow.webContents.executeJavaScript('location.reload()'))
-    })
+    //TODO: after updating electron to 21.3.5, Ctrl+R would stop working
     mainWindow.on('blur', () => {
         globalShortcut.unregister('CommandOrControl+R')
     })
-
+    mainWindow.on('focus', () => {
+        globalShortcut.register('CommandOrControl+R', () => mainWindow.webContents.executeJavaScript('location.reload()'))
+    })
     mainWindow.once('ready-to-show', () => {
         mainWindow.webContents.setZoomFactor(zoomFactor)
     })
@@ -60,6 +87,14 @@ async function createWindow() {
     await mainWindow.loadURL(`${serverHostname}`, { userAgent: 'ShooterGame/13 Windows/10.0.19043.1.256.64bit' })
 
     return mainWindow
+}
+
+function restoreMainWindow() {
+    if (!mainWindow) return
+
+    if (!mainWindow.isVisible()) mainWindow.show()
+    if (mainWindow.isMaximized()) mainWindow.restore()
+    if (!mainWindow.isFocused()) mainWindow.focus()
 }
 
 ipcMain.on('show', (event) => {
@@ -115,11 +150,7 @@ if (!SingleInstanceLock) {
     app.quit()
 } else {
     app.on('second-instance', () => {
-        if (!mainWindow) return
-
-        if (!mainWindow.isVisible()) mainWindow.show()
-        if (mainWindow.isMaximized()) mainWindow.restore()
-        if (!mainWindow.isFocused()) mainWindow.focus()
+        restoreMainWindow()
     })
 
     app.whenReady().then(async () => {
@@ -145,8 +176,13 @@ if (!SingleInstanceLock) {
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
-app.on('window-all-closed', function () {
-    if (process.platform !== 'darwin') app.quit()
+app.on('window-all-closed', () => {
+    app.quit()
+})
+
+app.on('will-quit', () => {
+    globalShortcut.unregisterAll()
+    // trayIcon.destroy()
 })
 
 const asarExists = async (resourceName): Promise<boolean> => {
