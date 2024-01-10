ed3f065d07bf5d6caf807cab66257d00efddf22a
RXJpaw <github@rxj.pw>
2024-01-10 22:44:48 +0100

    💬 Updated copyright year to 2024 in sidebar.

--- src/components/Structure/Sidebar.vue
+++ src/components/Structure/Sidebar.vue
@@ -34,7 +34,7 @@
             <StructureSidebarUpdate />
             <div class="footer">
                 <div class="version">Valorant Companion, Closed {{ version }}</div>
-                <div class="copyright">Copyright © 2023 RXJpaw</div>
+                <div class="copyright">Copyright © 2024 RXJpaw</div>
             </div>
         </div>
     </div>
